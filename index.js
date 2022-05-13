7d5e182e07f3d4c39a30d6d243f9a18886b2fb26
Adenola desmond <56209529+locksiDesmond@users.noreply.github.com>
2022-05-13 05:45:38 +0100

    initialize structure

---
+++ index.js
@@ -0,0 +1,12 @@
+const html = learn(html);
+const css = learn(css);
+
+buildWebsiteWithSkills(html, css);
+
+const javascript = learn(javascript);
+
+if (successfullyLearn(javascript)) {
+  const react = learn(react);
+  buildWebsiteWithSkills();
+}
+// to be continued
